6c452844f49ca2cef5140c2ef06d7fa486a92f0d
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-03-12 21:23:50 +0100

    Final commit.

--- src/main/java/pl/asilver/multiplication/_main/_Main.java
+++ src/main/java/pl/asilver/multiplication/_main/_Main.java
@@ -1,4 +1,9 @@
 package pl.asilver.multiplication._main;
 
+import static pl.asilver.multiplication.service.Service.multiplyNumbers;
+
 public class _Main {
+    public static void main(String[] args) {
+        multiplyNumbers();
+    }
 }
--- src/main/java/pl/asilver/multiplication/service/Service.java
+++ src/main/java/pl/asilver/multiplication/service/Service.java
@@ -1,10 +1,10 @@
 package pl.asilver.multiplication.service;
 
 public class Service {
-    public static void multiplyNumbers(int a, int b){
-        for(a = 2; a <= 5; a++){
-            for(b = 2; b <=5; b++){
-                System.out.println(a + " * " + " b " + " = " + (a*b));
+    public static void multiplyNumbers() {
+        for (int a = 2; a <= 5; a++) {
+            for (int b = 2; b <= 5; b++) {
+                System.out.println(a + " * " + b + " = " + (a * b));
             }
         }
     }
